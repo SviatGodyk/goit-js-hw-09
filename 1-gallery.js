6bf26c07b9b089a905a172a8960fcd633bf74467
SviatGodyk <godyk1992@gmail.com>
2025-01-22 13:10:47 +0000

    Deploying to gh-pages from @ SviatGodyk/goit-js-hw-09@44d36d7d5a38f08a75220d94e035d1c85718ad86 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import"./assets/styles-l0sNRNKZ.js";console.log("hi");
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import"./assets/styles-l0sNRNKZ.js";console.log("hi");
+//# sourceMappingURL=2-form.js.map
